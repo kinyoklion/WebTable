55dba5305376ea6737ed8e33f4cce2824c4c3aa2
kinyoklion <kingdewman@gmail.com>
2015-11-20 01:00:29 +0000

    Add the MapObject class. Add a list of map objects to the layer class.

--- MapData/layer.js
+++ MapData/layer.js
@@ -4,7 +4,7 @@
  * managing objects.
  */
  
- define(["MapData/observable"], function (observable) {
+ define(['MapData/observable', 'MapData/mapobject'], function (observable, mapobject) {
      
      /**
       * Construct a layer object. Layer objects are responsible for managing
@@ -13,32 +13,86 @@
       * object.
       */
      function Layer(opt_json) {
-         var visible = (opt_json === undefined) ? true : opt_json.visible;
-         var editorVisible = (opt_json === undefined) ? true : opt_json.editorVisible;
-         var viewerVisible = (opt_json === undefined) ? true : opt_json.viewerVisible;
-         var opacity = (opt_json === undefined) ? 1 : opt_json.opacity;
-         var name = (opt_json == undefined) ? 1 : opt_json.name;
-         
-         //TODO: RRL: Add object list.
-         
-         observable.MakeObservable(this);
-         
-         this.createObservedProperty("visible", visible);
-         this.createObservedProperty("editorVisible", editorVisible);
-         this.createObservedProperty("viewerVisible", viewerVisible);
-         this.createObservedProperty("opacity", opacity);
-         this.createObservedProperty("name", name);
+        var visible = (opt_json === undefined) ? true : opt_json.visible;
+        var editorVisible = (opt_json === undefined) ? true : opt_json.editorVisible;
+        var viewerVisible = (opt_json === undefined) ? true : opt_json.viewerVisible;
+        var opacity = (opt_json === undefined) ? 1 : opt_json.opacity;
+        var name = (opt_json == undefined) ? 1 : opt_json.name;
+        
+        var objects = [];
+        
+        if(opt_json !== undefined) {
+        for(var objectIndex = 0; objectIndex < opt_json.objects.length; objectIndex++) {
+            objects.push(new mapobject.fromJSON(opt_json.objects[objectIndex]));
+            }
+        }
+        
+        observable.MakeObservable(this);
+        
+        this.createObservedProperty("visible", visible);
+        this.createObservedProperty("editorVisible", editorVisible);
+        this.createObservedProperty("viewerVisible", viewerVisible);
+        this.createObservedProperty("opacity", opacity);
+        this.createObservedProperty("name", name);
+        
+        /**
+         * Add an object to the layer.
+         * @param {MapObject} objectToAdd The object to add to the layer.
+         */
+        this.addObject = function(objectToAdd) {
+            objects.push(objectToAdd);
+            this.addChildObservable("objects["+ String(objects.length - 1) +"]", objectToAdd);
+            this.notify("objects", objectToAdd, observable.ChangeType.ADDED);
+        };
+        
+        /**
+         * Remove the object at the specified index.
+         * @param {number} objectIndex The index of the object to remove.
+         */
+        this.removeObjectByIndex = function(objectIndex) {
+            if(objectIndex > objects.length || objectIndex < 0) {
+                throw new Error("Invalid object index: " + objectIndex);
+            }
+            var object = objects[objectIndex];
+            this.removeChildObservable(object);
+            objects.splice(objectIndex, 1);
+        };
+        
+        /**
+         * Get the object at the specified index.
+         * @param {number} objectIndex The index of the object to remove.
+         * @returns {MapObject} The object at the index.
+         */
+        this.getObjectByIndex = function(objectIndex) {
+            if(objectIndex > objects.length || objectIndex < 0) {
+                throw new Error("Invalid object index: " + objectIndex);
+            }
+            return objects[objectIndex];
+        };
+        
+        /**
+         * Get the number of objects in the layer.
+         * @returns {number} The number of objects in the layer.
+         */
+        this.getObjectCount = function() {
+            return objects.length;
+        };
         
         /**
          * Create a version of this object suitable for JSON serialization.
          */
         this.toJSON = function () {
+            var objectList = [];
+            for(var objectIndex = 0; objectIndex < objects.length; objectIndex++) {
+                objectList.push(objects[objectIndex].toJSON());
+            }
             return {
                 visible: this.visible,
                 editorVisible: this.editorVisible,
                 viewerVisible: this.viewerVisible,
                 opacity: this.opacity,
-                name: this.name
+                name: this.name,
+                objects: objectList
             };
         };
     }
---
+++ MapData/mapobject.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Ryan Lamb on 11/19/15.
+ * MapObjects are used to represent all objects within a map.
+ */
+ 
+ define(['MapData/observable', 'MapData/transform'], function(observable, transform) {
+    
+    /**
+     * Construct a MapObject.
+     * @param {object} [opt_json] Optional object containing a JSON serialized
+     * form.
+     */
+    function MapObject(opt_json) {
+        var objectTransform = (opt_json === undefined) ? new transform.Transform() :
+            transform.fromJSON(opt_json);
+        
+        observable.MakeObservable(this);
+
+        this.createObservableChildProperty("transform", objectTransform, "transform");
+        
+        this.toJSON = function() {
+            return {
+                transform: this.transform.toJSON()
+            };
+        };
+    }
+    
+    /**
+     * Construct a transform from a JSON representation.
+     * @param {object} jsonMapObject A JSON object version of the MapObject.
+     */
+    function fromJSON(jsonMapObject) {
+        return new MapObject(jsonMapObject);
+    }
+    
+    return {
+        MapObject: MapObject(),
+        fromJSON: fromJSON
+    };
+ });
