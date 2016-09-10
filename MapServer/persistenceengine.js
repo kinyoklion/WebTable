2a6ce88da58007e372e5283e2f743de188ab0232
kinyoklion <kingdewman@gmail.com>
2016-09-09 23:53:19 -0700

    Recursively resolve database promises.

--- MapServer/persistenceengine.js
+++ MapServer/persistenceengine.js
@@ -15,6 +15,7 @@ function PersistenceEngine(database, mapdata, observable) {
     //only have the new name, then we will not be able to find the record.
     var persistedName = null;
     var operations = [];
+    var pendingOperations = [];
 
     this.loadMap = function(mapName, callback) {
         var persistence = this;
@@ -54,9 +55,35 @@ function PersistenceEngine(database, mapdata, observable) {
      * @param {function} callback A callback that will execute after all pending operations have completed.
      */
     this.then = function(callback) {
-        Promise.all(operations).then(function(values) {
-            operations = [];
+        if(pendingOperations.length == 0)
+        {
             callback();
+        }
+
+        var operation = pendingOperations[0];
+        pendingOperations.splice(0, 1);
+
+        var change = operation.change;
+        var path = operation.path;
+        var value = operation.value;
+        var nameAtTime = operation.name;
+
+        var promise = null;
+
+        if (change === observable.ChangeType.UPDATED) {
+            promise = database.updateField(nameAtTime, path, value);
+        }
+        else if (change === observable.ChangeType.REMOVED) {
+            promise = database.removeItem(nameAtTime, path, value);
+        }
+        else if (change === observable.ChangeType.ADDED) {
+            promise = database.addItem(nameAtTime, path, value);
+        }
+
+        var localThis = this;
+
+        promise.then(function() {
+            localThis.then(callback)
         });
     };
 
@@ -68,15 +95,7 @@ function PersistenceEngine(database, mapdata, observable) {
      * @param {ChangeType} change The type of the change.
      */
     var updateMap = function(sender, path, value, change) {
-        if (change === observable.ChangeType.UPDATED) {
-            operations.push(database.updateField(persistedName, path, value));
-        }
-        else if (change === observable.ChangeType.REMOVED) {
-            operations.push(database.removeItem(persistedName, path, value));
-        }
-        else if (change === observable.ChangeType.ADDED) {
-            operations.push(database.addItem(persistedName, path, value));
-        }
+        pendingOperations.push({name: persistedName, change: change, path: path, value: value});
 
         //Store the name of the most recent database. This is needed if it
         //changes and doesn't hurt if it doesn't change.
